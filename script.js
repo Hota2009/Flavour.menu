7430f383018033ac6869cc66f3942f07a018eea1
ahmed idrees <112004714+Hota2009@users.noreply.github.com>
2025-05-02 16:13:52 +0300

    Update script.js

--- script.js
+++ script.js
@@ -181,7 +181,7 @@ const categories = [
             { id: 17, name: 'Blue Power', price: '95 EGP', image: '18.png', description: 'Ingredients: Red Bull, Blue Curacao, lemon, soda' },
             { id: 18, name: 'Passion Fruit Mojito', price: '60 EGP', image: '19.png', description: 'Ingredients: Passion fruit, lemon, soda' },
             { id: 19, name: 'Gum', price: '50 EGP', image: '20.png', description: 'Gum' },
-            { id: 20, name: 'Apple Mint', price: '65 EGP', image: '21.png', description: 'Ingredients: Apple, mint' }
+            { id: 20, name: 'Apple Mint', price: '65 EGP', image: '21.png', description: 'Ingredients: Apple, mint' },
             { id: 20, name: 'Watermelon Mocktail', price: '60 EGP', image: 'WhatsApp Image 2025-04-26 at 21.30.52_29528aec.jpg', description: 'Ingredients: watermelon,soda,lemon,mint' }
         ]
     }
